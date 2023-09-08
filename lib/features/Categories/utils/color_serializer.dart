8b10653ddfa65b15a5e3c3b6da89becbb2b8c4fc
Berke Uğur <flutterteam2023@gmail.com>
2023-09-08 16:49:49 +0300

    android/app/src/main/res update

--- lib/features/Categories/utils/color_serializer.dart
+++ lib/features/Categories/utils/color_serializer.dart
@@ -6,12 +6,14 @@ class ColorSerializer implements JsonConverter<Color, dynamic> {
   @override
   Color fromJson(dynamic colorName) {
     if (colorName != null) {
+      assert(colorName.runtimeType == String, "toJson value is not Color runtimetype");
       colorName as String;
       List<String> colorParameters = colorName.replaceAll(" ", "").split(",");
       final colorInt = <int>[];
       for (var colorParam in colorParameters) {
         colorInt.add(int.parse(colorParam));
       }
+      assert(colorInt.length == 4, "Incoming color data parameters length is lower than 4");
       return Color.fromARGB(colorInt[0], colorInt[1], colorInt[2], colorInt[3]);
     } else {
       return Colors.blue;
@@ -19,5 +21,9 @@ class ColorSerializer implements JsonConverter<Color, dynamic> {
   }
 
   @override
-  Color toJson(dynamic colorNama) => Colors.blue;
+  String toJson(dynamic color) {
+    assert(color.runtimeType == Color, "toJson value is not Color runtimetype");
+    color as Color;
+    return "${color.alpha}, ${color.red}, ${color.green}, ${color.blue}";
+  }
 }
