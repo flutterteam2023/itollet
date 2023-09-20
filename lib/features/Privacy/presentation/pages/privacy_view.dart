f78f138ab762393fdfde806a4d2ab0165341588a
Berke Uğur <berkeugur67@gmail.com>
2023-09-20 18:24:02 +0300

    profile icons

--- lib/features/Privacy/presentation/pages/privacy_view.dart
+++ lib/features/Privacy/presentation/pages/privacy_view.dart
@@ -5,6 +5,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:itollet/common_widgets/custom_appbar.dart';
 import 'package:itollet/common_widgets/element_ellipse.dart';
 import 'package:itollet/constants/constant_colors.dart';
+import 'package:itollet/constants/profile_icons.dart';
 import 'package:itollet/features/Drawer/drawer_view.dart';
 
 @RoutePage()
@@ -18,42 +19,37 @@ class PrivacyView extends ConsumerWidget {
       drawer: CustomDrawer(scaffoldKey: scaffoldKey),
       appBar: const CustomAppBar(),
       body: Padding(
-        padding:  EdgeInsets.only(top: 24.h),
+        padding: EdgeInsets.only(top: 24.h),
         child: Column(
           children: [
             PreferredSize(
+              preferredSize: const Size.fromHeight(kToolbarHeight),
+              child: PreferredSize(
                 preferredSize: const Size.fromHeight(kToolbarHeight),
-                child: PreferredSize(
-                  preferredSize: const Size.fromHeight(kToolbarHeight),
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: secondary
+                child: Container(
+                  decoration: const BoxDecoration(color: secondary),
+                  child: AppBar(
+                    automaticallyImplyLeading: false,
+                    leading: IconButton(
+                        onPressed: () {
+                          context.back();
+                        },
+                        icon: const Icon(Icons.arrow_back_ios)),
+                    forceMaterialTransparency: true,
+                    surfaceTintColor: Colors.white,
+                    foregroundColor: Colors.white,
+                    shadowColor: Colors.white,
+                    iconTheme: const IconThemeData(color: Colors.white),
+                    centerTitle: true,
+                    title: Text(
+                      'GİZLİLİK',
+                      style: TextStyle(color: Colors.white, fontSize: 24.sp, fontWeight: FontWeight.w400),
                     ),
-                    child: AppBar(
-                      automaticallyImplyLeading: false,
-                      leading: IconButton(onPressed:() {
-                        context.back();
-                        
-                      }, icon: const Icon(Icons.arrow_back_ios)),
-                      forceMaterialTransparency: true,
-                      surfaceTintColor: Colors.white,
-                      foregroundColor: Colors.white,
-                      shadowColor: Colors.white,
-                      iconTheme: const IconThemeData(color: Colors.white),
-                      centerTitle: true,
-                      title: Text(
-                        'GİZLİLİK',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24.sp,
-                            fontWeight: FontWeight.w400),
-                      ),
-                      backgroundColor:
-                          Colors.white, // Arkaplan rengini transparent yapın
-                    ),
+                    backgroundColor: Colors.white, // Arkaplan rengini transparent yapın
                   ),
                 ),
               ),
+            ),
             Padding(
               padding: EdgeInsets.only(top: 31.h),
               child: Center(
@@ -70,8 +66,7 @@ class PrivacyView extends ConsumerWidget {
     );
   }
 
-  Padding walletCard(
-      BuildContext context, GlobalKey<ScaffoldState> scaffoldKey) {
+  Padding walletCard(BuildContext context, GlobalKey<ScaffoldState> scaffoldKey) {
     return Padding(
       padding: EdgeInsets.only(left: 18.w, right: 18.w, top: 18.w),
       child: Wrap(
@@ -80,17 +75,19 @@ class PrivacyView extends ConsumerWidget {
         children: [
           ElementEllipse(
             title: 'HESABIMI SİL',
+            iconUrl: PrivacyIcons.delete,
             colors: const [Color(0xffFF553D), Color(0xffFF864B)],
-            onTap: () {
-            },
+            onTap: () {},
           ),
           ElementEllipse(
             title: 'GİZLİLİK POLİTİKASI',
+            iconUrl: PrivacyIcons.privacy,
             colors: const [Color(0xff5CC65D), Color(0xff2CA882)],
             onTap: () {},
           ),
           ElementEllipse(
             title: 'KULLANICI SÖZLEŞMESİ',
+            iconUrl: PrivacyIcons.user,
             colors: const [Color(0xff0046A5), Color(0xff50D7E0)],
             onTap: () {},
           ),
--- lib/features/Wallet/presentation/pages/wallet_view.dart
+++ lib/features/Wallet/presentation/pages/wallet_view.dart
@@ -1,15 +1,12 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:itollet/common_widgets/custom_appbar.dart';
-import 'package:itollet/common_widgets/custom_filled_button.dart';
 import 'package:itollet/common_widgets/element_ellipse.dart';
 import 'package:itollet/constants/constant_colors.dart';
+import 'package:itollet/constants/profile_icons.dart';
 import 'package:itollet/features/Drawer/drawer_view.dart';
-import 'package:itollet/features/Home/presentation/providers/home_notifier.dart';
-import 'package:itollet/routing/app_router.dart';
 
 @RoutePage()
 class WalletView extends ConsumerWidget {
@@ -22,42 +19,37 @@ class WalletView extends ConsumerWidget {
       drawer: CustomDrawer(scaffoldKey: scaffoldKey),
       appBar: const CustomAppBar(),
       body: Padding(
-        padding:  EdgeInsets.only(top: 24.h),
+        padding: EdgeInsets.only(top: 24.h),
         child: Column(
           children: [
             PreferredSize(
+              preferredSize: const Size.fromHeight(kToolbarHeight),
+              child: PreferredSize(
                 preferredSize: const Size.fromHeight(kToolbarHeight),
-                child: PreferredSize(
-                  preferredSize: const Size.fromHeight(kToolbarHeight),
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: secondary
+                child: Container(
+                  decoration: const BoxDecoration(color: secondary),
+                  child: AppBar(
+                    automaticallyImplyLeading: false,
+                    leading: IconButton(
+                        onPressed: () {
+                          context.back();
+                        },
+                        icon: const Icon(Icons.arrow_back_ios)),
+                    forceMaterialTransparency: true,
+                    surfaceTintColor: Colors.white,
+                    foregroundColor: Colors.white,
+                    shadowColor: Colors.white,
+                    iconTheme: const IconThemeData(color: Colors.white),
+                    centerTitle: true,
+                    title: Text(
+                      'CÜZDANIM',
+                      style: TextStyle(color: Colors.white, fontSize: 24.sp, fontWeight: FontWeight.w400),
                     ),
-                    child: AppBar(
-                      automaticallyImplyLeading: false,
-                      leading: IconButton(onPressed:() {
-                        context.back();
-                        
-                      }, icon: const Icon(Icons.arrow_back_ios)),
-                      forceMaterialTransparency: true,
-                      surfaceTintColor: Colors.white,
-                      foregroundColor: Colors.white,
-                      shadowColor: Colors.white,
-                      iconTheme: const IconThemeData(color: Colors.white),
-                      centerTitle: true,
-                      title: Text(
-                        'CÜZDANIM',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24.sp,
-                            fontWeight: FontWeight.w400),
-                      ),
-                      backgroundColor:
-                          Colors.white, // Arkaplan rengini transparent yapın
-                    ),
+                    backgroundColor: Colors.white, // Arkaplan rengini transparent yapın
                   ),
                 ),
               ),
+            ),
             Padding(
               padding: EdgeInsets.only(top: 31.h),
               child: Center(
@@ -74,8 +66,7 @@ class WalletView extends ConsumerWidget {
     );
   }
 
-  Padding walletCard(
-      BuildContext context, GlobalKey<ScaffoldState> scaffoldKey) {
+  Padding walletCard(BuildContext context, GlobalKey<ScaffoldState> scaffoldKey) {
     return Padding(
       padding: EdgeInsets.only(left: 18.w, right: 18.w, top: 18.w),
       child: Wrap(
@@ -84,17 +75,19 @@ class WalletView extends ConsumerWidget {
         children: [
           ElementEllipse(
             title: 'BAKİYE',
+            iconUrl: WalletIcons.balance,
             colors: const [Color(0xffFF553D), Color(0xffFF864B)],
-            onTap: () {
-            },
+            onTap: () {},
           ),
           ElementEllipse(
             title: 'BAKİYE\nYÜKLE',
+            iconUrl: WalletIcons.addBalance,
             colors: const [Color(0xff5CC65D), Color(0xff2CA882)],
             onTap: () {},
           ),
           ElementEllipse(
             title: 'HESAP HAREKETLERİ',
+            iconUrl: WalletIcons.accountActivities,
             colors: const [Color(0xff0046A5), Color(0xff50D7E0)],
             onTap: () {},
           ),
