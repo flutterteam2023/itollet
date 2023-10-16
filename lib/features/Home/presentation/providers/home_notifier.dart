9b86c21605c4fa185d00845560d05aab7d4c1595
Berke Uğur <berkeugur67@gmail.com>
2023-10-16 19:28:30 +0300

    Teklif Verme Fonksiyonu Değişikliği
    Teklif ver butonuna hesap hareketleri servisinin fonksiyonları eklendi

--- lib/features/Home/presentation/providers/home_notifier.dart
+++ lib/features/Home/presentation/providers/home_notifier.dart
@@ -4,12 +4,17 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:itollet/features/Account/Models/account_transaction_model.dart';
+import 'package:itollet/features/Account/Services/account_transaction_service.dart';
+import 'package:itollet/features/Account/Utils/account_transaction_type_enum.dart';
 import 'package:itollet/features/Auth/Login/data/model/user_model.dart';
 import 'package:itollet/features/Categories/models/post_model/post_model.dart';
 import 'package:itollet/features/Home/presentation/states/home_state.dart';
+import 'package:itollet/iberkeugur/Log/log.dart';
 import 'package:itollet/iberkeugur/Snackbar/snackbar_extension.dart';
 
-final homeProvider = NotifierProvider.autoDispose<HomeNotifier, HomeState>(HomeNotifier.new);
+final homeProvider =
+    NotifierProvider.autoDispose<HomeNotifier, HomeState>(HomeNotifier.new);
 
 class HomeNotifier extends AutoDisposeNotifier<HomeState> {
   final _auth = FirebaseAuth.instance;
@@ -17,7 +22,6 @@ class HomeNotifier extends AutoDisposeNotifier<HomeState> {
   @override
   HomeState build() {
     getUser();
-       
 
     return HomeState.initial();
   }
@@ -28,7 +32,10 @@ class HomeNotifier extends AutoDisposeNotifier<HomeState> {
       await _auth.currentUser?.updateDisplayName(newUserName);
       await getUser();
       UserModel user = state.user;
-      await FirebaseFirestore.instance.collection("users").doc(_auth.currentUser?.uid).update(
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(_auth.currentUser?.uid)
+          .update(
             user.copyWith(userName: newUserName).toJson(),
           );
       isSuccess = true;
@@ -47,7 +54,8 @@ class HomeNotifier extends AutoDisposeNotifier<HomeState> {
           .collection('users')
           .doc(_auth.currentUser?.uid)
           .withConverter<UserModel>(
-            fromFirestore: (snapshot, _) => UserModel.fromJson(snapshot.data()!),
+            fromFirestore: (snapshot, _) =>
+                UserModel.fromJson(snapshot.data()!),
             toFirestore: (model, _) => model.toJson(),
           )
           .get()
@@ -70,17 +78,16 @@ class HomeNotifier extends AutoDisposeNotifier<HomeState> {
       await db
           .collection('posts')
           .withConverter<PostModel>(
-            fromFirestore: (snapshot, _) => PostModel.fromJson(snapshot.data()!),
+            fromFirestore: (snapshot, _) =>
+                PostModel.fromJson(snapshot.data()!),
             toFirestore: (model, _) => model.toJson(),
           )
           .get()
           .then((value) {
         for (var doc in value.docs) {
-         
           list.add(doc.data());
-          if (doc.data().fromUID==_auth.currentUser?.uid) {
+          if (doc.data().fromUID == _auth.currentUser?.uid) {
             myList.add(doc.data());
-            
           }
         }
       });
@@ -88,139 +95,158 @@ class HomeNotifier extends AutoDisposeNotifier<HomeState> {
       print(e.toString());
     }
     state = state.copyWith(postModels: list);
-      state = state.copyWith(myPostList: myList);
-
+    state = state.copyWith(myPostList: myList);
 
     return list;
   }
-  Future<void> postUrl(String postId,String url,BuildContext context)async{
-    if (state.streamUser.balance!=0 || state.streamUser.balance!<0) {
-       final list = <String>[];
-    
+
+  Future<void> postUrl(String postId, String url, BuildContext context) async {
+    if (state.streamUser.balance != 0 || state.streamUser.balance! < 0) {
+      final list = <String>[];
+
+      final db = FirebaseFirestore.instance;
+      await db.collection('posts').doc(postId).get().then((value) {
+        if (value.data()!["postUrl"] != null) {
+          for (var e in value.data()!["postUrl"]) {
+            list.add(e);
+          }
+        }
+      }).then((value) {
+        db
+            .collection('users')
+            .doc(FirebaseAuth.instance.currentUser?.uid)
+            .update({'balance': state.streamUser.balance! - 3.75});
+      });
+
+      list.add(url);
+      try {
+        await db
+            .collection('posts')
+            .doc(postId)
+            .update({'postUrl': list}).then((value) {
+          context.popRoute();
+          context.snackbar('Teklif Verild!',
+              backgroundColor: Colors.pink,
+              contentStyle: TextStyle(color: Colors.white, fontSize: 15.sp));
+        });
+        list.clear();
+      } catch (e) {
+        print(e);
+      }
+
+      try {
+        await AccountTransactionService().create(
+          AccountTransactionModel(
+            amount: "- 3.75₺",
+            fromUid: FirebaseAuth.instance.currentUser?.uid ?? "null",
+            createdAt: Timestamp.now().toDate(),
+            docId: "docId",
+            type: AccountTransactionTypes.biddingFee,
+          ),
+        );
+      } catch (e) {
+        Log.instance.error(
+            "AccountTransactionService create failed in home_notifier.dart");
+      }
+    } else {
+      context.popRoute();
+      context.snackbar('Lütfen Bakiye Yükleyin!',
+          backgroundColor: Colors.pink,
+          contentStyle: TextStyle(color: Colors.white, fontSize: 15.sp));
+    }
+  }
+
+  Stream<List<String>> getPostUrlStream(String postId) async* {
     final db = FirebaseFirestore.instance;
-   await db.collection('posts').doc(postId).get().then((value){
-    if (value.data()!["postUrl"]!=null) {
-       for (var e in value.data()!["postUrl"]) {
+    final list = <String>[];
+
+    try {
+      final value = await db.collection('posts').doc(postId).get();
+      for (var e in value.data()!["postUrl"]!) {
         list.add(e);
       }
-      
-    }
-     
-      
-    }).then((value) {
-      db.collection('users').doc(FirebaseAuth.instance.currentUser?.uid).update({
-        'balance':state.streamUser.balance! - 3.75
-      });
-    });
- 
-    list.add(url);
+      state = state.copyWith(postUrls: list);
+    } catch (e) {}
+
+    yield state.postUrls;
+  }
+
+  Future<void> addBalance(double balance, BuildContext context) async {
     try {
-      await db
-      .collection('posts')
-      .doc(postId)
-      .update({
-        'postUrl':list
-      }).then((value) {
-        context.popRoute();
-      context.snackbar('Teklif Verild!',backgroundColor: Colors.pink,contentStyle: TextStyle(
-        color: Colors.white,
-        fontSize: 15.sp
+      if (state.streamUser.balance != null) {
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(FirebaseAuth.instance.currentUser?.uid)
+            .update({'balance': state.streamUser.balance! + balance}).then(
+                (value) {
+          context.snackbar('Bakiye Yüklendi!',
+              backgroundColor: Colors.pink,
+              contentStyle: TextStyle(color: Colors.white, fontSize: 15.sp));
+        });
+      } else {
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(FirebaseAuth.instance.currentUser?.uid)
+            .update({'balance': state.streamUser.balance ?? 0 + balance}).then(
+                (value) {
+          context.snackbar('Bakiye Yüklendi!',
+              backgroundColor: Colors.pink,
+              contentStyle: TextStyle(color: Colors.white, fontSize: 15.sp));
+        });
+      }
+      //hesap hareketlerini kayıt etme
+
+      await AccountTransactionService().create(AccountTransactionModel(
+        amount: "+$balance ₺",
+        fromUid: (FirebaseAuth.instance.currentUser?.uid).toString(),
+        createdAt: Timestamp.now().toDate(),
+        docId: "",
+        type: AccountTransactionTypes.addBalance,
       ));
-      }); 
-      list.clear();
     } catch (e) {
-      print(e); 
+      // ignore: use_build_context_synchronously
+      context.snackbar('Hata Oluştu $e',
+          backgroundColor: Colors.pink,
+          contentStyle: TextStyle(color: Colors.white, fontSize: 15.sp));
     }
-      
-    }else{
-      context.popRoute();
-      context.snackbar('Lütfen Bakiye Yükleyin!',backgroundColor: Colors.pink,contentStyle: TextStyle(
-        color: Colors.white,
-        fontSize: 15.sp
-      ));
+  }
+
+  Stream<UserModel> getStreamUser() async* {
+    final db = FirebaseFirestore.instance;
+
+    try {
+      final value = await db
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser?.uid)
+          .get();
+
+      if (value.exists) {
+        // value.data() artık Generic tipi içine alınarak kullanılmalıdır
+        final userData = value.data() as Map<String, dynamic>;
+
+        // UserModel sınıfını oluşturduğunuzu varsayalım ve bu veriyi kullanarak nesne oluşturun
+        final userModel = UserModel.fromJson(userData);
+
+        // State'i güncelle
+        state = state.copyWith(streamUser: userModel);
+      }
+    } catch (e) {
+      // Hata durumunda bir şeyler yapabilirsiniz
     }
-   
 
-  }
-  Stream<List<String>> getPostUrlStream(String postId) async* {
-    
-  final db = FirebaseFirestore.instance;
-  final list = <String>[];
-
-  try {
-    final value = await db
-        .collection('posts')
-        .doc(postId)
-        
-        .get();
-for (var e in value.data()!["postUrl"]!) {
-  list.add(e);
-  
-}
-state =state.copyWith(postUrls: list);
- 
-  } catch (e) {
+    yield state.streamUser;
   }
 
-  yield state.postUrls;
-}
-Future<void> addBalance(double balance,BuildContext context)async{
-  if (state.streamUser.balance!=null) {
-     await FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser?.uid).update({
-    
-    'balance':state.streamUser.balance! + balance
-  }).then((value){
-    context.snackbar('Bakiye Yüklendi!',backgroundColor: Colors.pink,contentStyle: TextStyle(
-      color: Colors.white,
-      fontSize: 15.sp
-    ));
-  });
-  }else{
-     await FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser?.uid).update({
-    
-    'balance':state.streamUser.balance??0 + balance
-  }).then((value) {
-    context.snackbar('Bakiye Yüklendi!',backgroundColor: Colors.pink,contentStyle: TextStyle(
-      color: Colors.white,
-      fontSize: 15.sp
-    ));
-  });
-  }
- 
-
-}
-
-   Stream<UserModel> getStreamUser() async* {
-  final db = FirebaseFirestore.instance;
-
-  try {
-    final value = await db
-        .collection('users')
-        .doc(FirebaseAuth.instance.currentUser?.uid)
-        .get();
-
-    if (value.exists) {
-      // value.data() artık Generic tipi içine alınarak kullanılmalıdır
-      final userData = value.data() as Map<String, dynamic>;
-
-      // UserModel sınıfını oluşturduğunuzu varsayalım ve bu veriyi kullanarak nesne oluşturun
-      final userModel = UserModel.fromJson(userData);
-
-      // State'i güncelle
-      state = state.copyWith(streamUser: userModel);
-    }
-  } catch (e) {
-    // Hata durumunda bir şeyler yapabilirsiniz
-  }
-
-  yield state.streamUser;
-}
-Stream<List<String>> getPostUrlsStream(String documentId) {
+  Stream<List<String>> getPostUrlsStream(String documentId) {
     // Firestore bağlantısı
     FirebaseFirestore firestore = FirebaseFirestore.instance;
 
     // Belirli bir dokümanı referans al ve 'postUrl' alanındaki değişiklikleri dinle
-    return firestore.collection('posts').doc(documentId).snapshots().map((snapshot) {
+    return firestore
+        .collection('posts')
+        .doc(documentId)
+        .snapshots()
+        .map((snapshot) {
       // Doküman varsa, 'postUrl' alanındaki verileri List<String> türüne dönüştür
       List<dynamic> urlList = snapshot.data()?['postUrl'] ?? [];
       return urlList.map((url) => url.toString()).toList();
